3312326c69c7ae9a2a512dd9e2def9f320930292
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-05-25 16:01:17 -0400

    added modern

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,4 +1,6 @@
-def to_roman(num):
+# Lazy Way:
+
+def to_roman_lazy(num):
     roman_dict= {
         "M": 1000,
         "D": 500,
@@ -19,4 +21,38 @@ def to_roman(num):
             divisible= num/arabic
     return str
 
-print(to_roman(424))
+# print(to_roman_lazy(424))
+
+
+# Modern Way:
+
+# Lazy Way:
+
+def to_roman(num):
+    roman_dict= {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX":9,
+        "V": 5,
+        "IV":4,
+        "I": 1,
+    }
+    str=""
+
+    for roman in roman_dict.keys():
+        arabic=roman_dict[roman]
+        divisible= num/arabic               #divisible is its own variable, so num/arabic are not being reevaluated when num is changing from while loop.
+        while divisible>=1:                 #could also get rid of the divisible variable completely and put while num/arabic>1 because this part DOES get revaluated
+            str+=roman
+            num-=arabic
+            divisible= num/arabic
+    return str
+
+# print(to_roman(424))
